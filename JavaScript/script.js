af468658fac7af41fc2875f91d514912f94b230c
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2024-11-16 21:31:41 +0100

    deplace index.html to the main folder and test data

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,27 +1,26 @@
-var questionsAll = ["5+5", "8*2", "10*10", "3*3"];
-var ques1rep = ["5", "10", "100", "8"];
-var ques2rep = ["10", "18", "16", "7"];
-var ques3rep = ["9", "16.5", "22", "100"];
-var ques4rep = ["6", "16", "8.5", "9"];
+fetch("../data.json")
+    .then((response) => response.json())
+    .then((data) => displayquestion(data.question1))
 
-var question = document.getElementById("question-qtnt");
-var reponse1 = document.getElementById("reponse1");
-var reponse2 = document.getElementById("reponse2");
-var reponse3 = document.getElementById("reponse3");
-var reponse4 = document.getElementById("reponse4");
-var next = document.getElementById("next");
+function displayquestion(info){
+    const allServ = document.getElementById("reponse");
+    allServ.innerHTML = info.map((item)=>{
+        return `
+        <button class="border-solid border-2 border-white p-10 font-[poppins] mb-5">${item.rep1}</button>
+        <button class="border-solid border-2 border-white p-10 font-[poppins] mb-5">${item.rep2}</button>
+        <button class="border-solid border-2 border-white p-10 font-[poppins] mb-5">${item.rep3}</button>
+        <button class="border-solid border-2 border-white p-10 font-[poppins] mb-5">${item.rep4}</button>
+        `
 
-var i = 0;
+    })
+    .join( ``);
+    const question = document.getElementById("question");
+    question.innerHTML = info.map((item)=>{
+        return `
+        
+        <h1 class="flex justify-center text-5xl mb-5 text-orange-500 font-bold font-[poppins]">${item.questions}</h1>
+        `
 
-next.addEventListener("click", function(){
-    next.innerHTML = ("Next ->");
-    question.innerHTML = (questionsAll[i]);
-    reponse1.innerHTML = (ques1rep[i]);
-    reponse2.innerHTML = (ques2rep[i]);
-    reponse3.innerHTML = (ques3rep[i]);
-    reponse4.innerHTML = (ques4rep[i]);
-    i++;
-});
-
-    
-
+    })
+    .join( ``);
+}
